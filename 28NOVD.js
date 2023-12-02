aee6d85354cb2916456e31a67932f1e8f3fa0e10
Vikram Singh Rana <116163333+vikramintech@users.noreply.github.com>
2023-12-02 15:26:00 +0000

    final code of 28NOVD

---
+++ 28NOVD.js
@@ -0,0 +1,9 @@
+console.clear();
+
+//sample array
+const fruits = "apple orange grapes kiwi guavava plums dragonfruit".split(" ");
+
+//implementation of implicit return type in an arrow function
+const getArrayLength = (array) => array.length;
+
+console.log(getArrayLength(fruits));
